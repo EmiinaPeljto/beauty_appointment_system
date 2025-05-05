044d6370bc5305361db428818064d42f1942ce7c
EmiinaPeljto <emina.peljto@stu.ibu.edu.ba>
2025-05-05 22:43:54 +0200

    LogIn page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home.jsx';
 import Profile from './pages/Profile.jsx';
+import SignUp from './pages/SignUp.jsx';
+import "./app.css"
 
 function App() {
   return (
@@ -9,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/profile" element={<Profile />} />
+        <Route path="/signup" element={<SignUp />} />
       </Routes>
     </Router>
   );
