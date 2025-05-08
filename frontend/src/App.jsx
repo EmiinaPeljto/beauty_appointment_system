de86089a87f2c1358a10ac47d38c0c2a0e37592e
EmiinaPeljto <emina.peljto@stu.ibu.edu.ba>
2025-05-08 17:47:49 +0200

    Combine navbar and footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,22 +5,26 @@ import Profile from './pages/Profile.jsx';
 import LogIn from './pages/LogIn';
 import SignUp from './pages/SignUp';
 import Navbar from './components/NavBar.jsx';
+import Footer from './components/Footer.jsx'; // Import Footer component
 import "./app.css";
 
 function App() {
   const location = useLocation(); // Get the current route
 
   return (
-    <>
+    <div className="flex flex-col min-h-screen">
       {/* Render Navbar only on the "/" route */}
-      {location.pathname === "/" && <Navbar />}
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/login" element={<LogIn />} />
-        <Route path="/signup" element={<SignUp />} />
-      </Routes>
-    </>
+      {location.pathname === '/' && <Navbar />}
+      <div className="flex-grow">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/login" element={<LogIn />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
+      </div>
+      {location.pathname !== '/login' && location.pathname !== '/signup' && <Footer />}
+    </div>
   );
 }
 
--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { FiLogIn } from "react-icons/fi"; // Importing an icon for the LogIn button
+import { FiLogIn } from "react-icons/fi";
+import Logo from "../assets/images/logo.png";
 
 const Navbar = () => {
   return (
     <nav className="flex bg-white justify-between items-center shadow p-4 bg-gray-800 text-black">
       {/* Logo and App Name on the Left */}
       <div className="flex items-center">
-        <img alt="Logo" src="../vite.svg" className="h-8 w-auto" />
-        <span className="ml-2 text-2xl font-bold ">GlamifyMe</span>
+        <img alt="Logo" src={Logo} className="h-12 w-auto" />
+        <div className="ml-2">
+          <span className="text-2xl font-sans italic">GlamifyMe</span>
+          <span className="block text-xs font-sans text-gray-500">
+            Beauty Appointment System
+          </span>
+        </div>
       </div>
 
       {/* Links in the Middle */}
@@ -16,16 +22,10 @@ const Navbar = () => {
         <Link to="/" className="text-base hover:text-[#FF66B2]">
           Home
         </Link>
-        <Link
-          to="/"
-          className="text-base text-base hover:text-[#FF66B2]"
-        >
+        <Link to="/" className="text-base text-base hover:text-[#FF66B2]">
           Services
         </Link>
-        <Link
-          to="/"
-          className="text-base text-base hover:text-[#FF66B2]"
-        >
+        <Link to="/" className="text-base text-base hover:text-[#FF66B2]">
           Help
         </Link>
       </div>
