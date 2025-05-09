6209be755d32acd41aeb05a34b4df6da47195b7e
samirazeba <samira.zeba@stu.ibu.edu.ba>
2025-05-09 20:38:35 +0200

    Salon Card frontend only

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,10 +6,11 @@ import LogIn from './pages/LogIn';
 import SignUp from './pages/SignUp';
 import Navbar from './components/NavBar.jsx';
 import Footer from './components/Footer.jsx'; // Import Footer component
+import Help from './pages/Help.jsx';
+import Services from './pages/Services.jsx'; // Import Services component
 
 import "./app.css";
 
-import Help from './pages/Help.jsx';
 
 function App() {
   const location = useLocation(); // Get the current route
@@ -17,14 +18,15 @@ function App() {
   return (
     <div className="flex flex-col min-h-screen">
       {/* Render Navbar only on the "/" route */}
-      {location.pathname === '/' && <Navbar />}
+      {location.pathname !== '/login' && location.pathname !== '/signup' && <Navbar />}
       <div className="flex-grow">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="/login" element={<LogIn />} />
           <Route path="/signup" element={<SignUp />} />
-          <Route path="/help" element={<Help />} /> {/* Assuming Help is the same as Home */}
+          <Route path="/help" element={<Help />} /> 
+          <Route path="/services" element={<Services />} />
         </Routes>
       </div>
       {location.pathname !== '/login' && location.pathname !== '/signup' && <Footer />}
---
+++ frontend/src/pages/Services.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import SalonCard from "../components/SalonCard";
+
+const Services = () => {
+  
+  return (
+    <div>
+        <div className="flex flex-wrap justify-center">
+            <SalonCard />
+        </div>
+    </div>
+  );
+};
+
+export default Services;
